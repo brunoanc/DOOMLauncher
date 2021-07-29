b963ca9fd189b965e3545c01482accafc4c4b0cc
PowerBall253 <brunoanconasala@gmail.com>
2021-07-28 23:25:33 -0500

    Upload new source

---
+++ DOOMLauncher/DoomLauncher.cs
@@ -0,0 +1,62 @@
+﻿using System.Diagnostics;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Memory;
+
+namespace DoomLauncher
+{
+    class DoomLauncher
+    {
+        // Memory object for DOOM process memory handling
+        private static Mem Memory = new Mem();
+
+        private static void Main(string[] args)
+        {
+            // Launch doom with the passed arguments
+            string launchArgs = String.Join(" ", args);
+            var doom = Process.Start("DOOMx64vk.exe", launchArgs);
+
+            if (doom == null)
+            {
+                Console.WriteLine("Failed to open the DOOM process!");
+                Environment.Exit(1);
+            }
+
+            // Wait for the original Steam process to exit
+            doom.WaitForExit();
+
+            // Wait for the DOOM process to be re-opened
+            int pId = 0;
+            SpinWait.SpinUntil(() => (pId = Memory.GetProcIdFromName(("DOOMx64vk"))) != 0);
+
+            if (Memory.OpenProcess(pId))
+            {
+                // Wait until the memory is loaded
+                SpinWait.SpinUntil(() =>
+                {
+                    var bytesRead = Memory.ReadBytes("DOOMx64vk.exe+18a31d0", 7);
+
+                    if (bytesRead == null)
+                    {
+                        return false;
+                    }
+
+                    return BitConverter.ToString(bytesRead) == "0F-B6-81-89-4C-03-00";
+                });
+
+                // Apply the patch and close the process
+                Memory.WriteMemory("DOOMx64vk.exe+18a31d0", "bytes", "31 C0 90 90 90 90 90");
+                Memory.CloseProcess();
+
+                Console.WriteLine("DOOM process has been patched successfully.");
+                Environment.Exit(0);
+            }
+            else
+            {
+                Console.WriteLine("Failed to patch the DOOM process!");
+                Environment.Exit(1);
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,15 @@
+# DOOMLauncher
+
+Launcher for DOOM (2016) that bypasses dev mode limitations on startup.
+
+## Usage
+
+Put "DOOMLauncher.exe" in your DOOM folder, then double click to run it.
+
+## Credits
+
+* ByteMe: Creator of the Cheat Engine assembly script this launcher is based on.
+
+* chrispy: Discovered the bypass that was used by ByteMe to create the auto assembly script.
+
+* erfg12: Creator of the memory.dll library.
